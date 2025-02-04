0e841c0bc4091deb5271a493ee6877cc5dad5e87
Liam Stone <lst131@uclive.ac.nz>
2025-02-04 17:38:29 +1300

    Zenith coordinate calculator working!

--- 00_StartupTests.R
+++ 00_StartupTests.R
@@ -1,8 +1,19 @@
 library(photobiology)
 library(lubridate)
 library(astroFns)
+library(httr)
+library(jsonlite)
+library(swephR)
+library(reticulate)
 
-dec_form(4, 40, 07)
+reticulate::py_install("skyfield")
+skyfield <- import("skyfield.api")
+
+
+planets <- skyfield$load("de440s.bsp")
+
+
+
 
 dec_form <- function(ra_h, ra_m, ra_s) {
   if(ra_h >= 0) {
@@ -14,12 +25,22 @@ dec_form <- function(ra_h, ra_m, ra_s) {
   result
 }
 
+time_to_long <- function(lst_deg, gst_deg) {
+  calc_long <- lst_deg - gst_deg
+  if(calc_long > 180) {
+    calc_long <- calc_long - 360
+  }
+  if(calc_long < -180) {
+    calc_long <- calc_long + 360
+  }
+  calc_long * 15
+}
+
 hour_angle <- function(local_s_time, ra_deg) {
   lha <- local_s_time - ra_deg
   if(lha < 0) {
     lha <- lha + 24
   }
-  print(lha)
   lha * 15
 }
 
@@ -27,22 +48,38 @@ alt <- function(dec, lat, H) {
   asin(sin(lat*(pi/180)) * sin(dec*(pi/180)) + cos(lat*(pi/180)) * cos(dec*(pi/180)) * cos(H*(pi/180)))
 }
 
-visibility <- function(ra_h, ra_m, ra_s, dec_h, dec_m, dec_s, obs_long, 
-                       obs_lat) {
-  ra_deg <- dec_form(ra_h, ra_m, ra_s)
-  dec_deg <- dec_form(dec_h, dec_m, dec_s)
+visibility <- function(which_planet, obs_long, obs_lat) {
+  ts <- skyfield$load$timescale()
+  t <- ts$now()
+  planet <- planets[which_planet]
+  earth <- planets['earth']
+  astrometric <- earth$at(t)$observe(planet)
+  coords <- astrometric$radec()
+  ra_deg <- coords[[1]]
+  dec_deg <- coords[[2]]
+  ra_deg <- ra_deg$hours
+  dec_deg <- dec_deg$degrees
   
   local_sidereal_time <- ut2lst(Sys.time(), lon.obs = obs_long)
   local_sidereal_time <- format(local_sidereal_time, format="%Y")
   local_sidereal_time <- as.numeric(local_sidereal_time)
+  greenwich_sidereal_time <- ut2lst(Sys.time(), lon.obs = 0)
+  greenwich_sidereal_time <- format(greenwich_sidereal_time, format="%Y")
+  greenwich_sidereal_time <- as.numeric(greenwich_sidereal_time)
+  
+  gst_deg <- greenwich_sidereal_time
+  
+  calculated_long <- time_to_long(ra_deg, gst_deg)
+  print(calculated_long)
+  print(dec_deg)
   
   H <- hour_angle(local_sidereal_time, ra_deg)
-
+  
   altitude <- alt(dec_deg, obs_lat, H)
   print(altitude * (180/pi))
 }
 
-visibility(4, 40, 07, 21, 38, 31, 172.569395, -43.538842)
+visibility('mars barycenter', -92.47437, 26.21957)
 
 
 
